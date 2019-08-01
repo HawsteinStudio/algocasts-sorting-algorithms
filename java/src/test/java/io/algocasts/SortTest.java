369c2ec470a96d5fac4e24524d78be7c0df8947d
Hawstein <tomhawstein@gmail.com>
2019-08-01 15:02:32 +0200

    add more special test cases

--- java/src/test/java/io/algocasts/SortTest.java
+++ java/src/test/java/io/algocasts/SortTest.java
@@ -8,7 +8,7 @@ import java.util.function.Consumer;
 
 class SortTest {
   private final int CNT = Helper.getRandomInt(10, 100);
-  private final int LENGTH = Helper.getRandomInt(20, 200);
+  private final int LENGTH = Helper.getRandomInt(1, 200);
 
   // some special cases
   private final List<int[]> specialArrays = Arrays.asList(
@@ -17,9 +17,18 @@ class SortTest {
     new int[]{0,1},  // 两个元素，递增
     new int[]{1,0},  // 两个元素，递减
     new int[]{1,1},  // 两个元素，相同
-    Helper.getSameElementArray(LENGTH, 42),  // LENGTH 个元素，相同
-    Helper.getSortedArray(LENGTH),           // LENGTH 个元素，递增
-    Helper.getReverseSortedArray(LENGTH)     // LENGTH 个元素，递减
+    new int[]{0,1,2},  // 三个元素，递增
+    new int[]{2,1,0},  // 三个元素，递减
+    new int[]{1,1,1},  // 三个元素，相同
+    Helper.getSameElementArray(LENGTH, 42),      // LENGTH 个元素，相同
+    Helper.getSortedArray(LENGTH),               // LENGTH 个元素，递增
+    Helper.getReverseSortedArray(LENGTH),        // LENGTH 个元素，递减
+    Helper.getSameElementArray(2*LENGTH, 42),    // 2*LENGTH 个元素(偶数个元素)，相同
+    Helper.getSortedArray(2*LENGTH),             // 2*LENGTH 个元素(偶数个元素)，递增
+    Helper.getReverseSortedArray(2*LENGTH),      // 2*LENGTH 个元素(偶数个元素)，递减
+    Helper.getSameElementArray(2*LENGTH+1, 42),  // 2*LENGTH+1 个元素(奇数个元素)，相同
+    Helper.getSortedArray(2*LENGTH+1),           // 2*LENGTH+1 个元素(奇数个元素)，递增
+    Helper.getReverseSortedArray(2*LENGTH+1)     // 2*LENGTH+1 个元素(奇数个元素)，递减
   );
 
   void test(Consumer<int[]> sort) {
