66541068ae60c8e558033b361aef39a67d7fb4f5
ray1888 <alal1995@hotmail.com>
2019-09-24 11:54:33 +0800

    <implement><heapsort gopher way>
    add heapsort in more gopher way in minHeap
    
    signed-off-by: ray1888

--- go/src/algocasts/HeapSort.go
+++ go/src/algocasts/HeapSort.go
@@ -1,5 +1,51 @@
 package algocasts
 
+import (
+	"container/heap"
+)
+
+// An IntHeap is a min-heap of ints.
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func HeapSortGolangImp(input []int) []int {
+	if len(input) == 0 {
+		return nil
+	}
+	minHeap := make(IntHeap, 0, len(input))
+	minHeapP := &minHeap
+	heap.Init(minHeapP)
+	for i := 0; i < len(input); i++ {
+		heap.Push(minHeapP, input[i])
+		//fmt.Printf("minHeapP is %v", minHeapP)
+	}
+	result := make([]int, len(input))
+	for i := 0; i < len(input); i++ {
+		minVal := heap.Pop(minHeapP)
+		result[i] = minVal.(int)
+	}
+	return result
+}
+
+// not use container/heap implement
+
 func shifDown(arr []int, i, end int) {
 	parent := i
 	child := 2*parent + 1
--- go/src/algocasts/HeapSort_test.go
+++ go/src/algocasts/HeapSort_test.go
@@ -2,6 +2,7 @@ package algocasts
 
 import (
 	"github.com/stretchr/testify/assert"
+
 	"testing"
 )
 
@@ -18,5 +19,22 @@ func TestHeapSort(t *testing.T) {
 	for _, tt := range tests {
 		HeapSort(tt.input)
 		assert.Equal(t, tt.input, tt.want)
+	}
+}
+
+func TestHeapSortGolang(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []int
+		want  []int
+	}{
+		{"double", []int{2, 1}, []int{1, 2}},
+		{"single", []int{1, 5, 4}, []int{1, 4, 5}},
+		{"normal", []int{1, 9, 5, 3, 2, 6}, []int{1, 2, 3, 5, 6, 9}},
+	}
+	for _, tt := range tests {
+		actual := HeapSortGolangImp(tt.input)
+		//fmt.Printf("actual is %v", actual)
+		assert.Equal(t, actual, tt.want)
 	}
 }
